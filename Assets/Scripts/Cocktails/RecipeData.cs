7a9cd19685d7e8405ed0adcebb67ca4e8d031617
BoranCem <borancemgoksu@gmail.com>
2026-09-24 18:19:33 +0300

    Add cocktail book data to recipes

--- Assets/Scripts/Cocktails/RecipeData.cs
+++ Assets/Scripts/Cocktails/RecipeData.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Recipe", menuName = "Bar Game/Recipe")]
 public class RecipeData : ScriptableObject
 {
+    [Header("Recipe Information")]
     public string recipeName;
 
+    public Sprite cocktailIcon;
+
+    [TextArea(3, 8)]
+    public string cocktailStory;
+
+    [Header("Ingredients")]
     public IngredientData primary;
-
     public IngredientData secondary;
-
     public IngredientData third;
 }
